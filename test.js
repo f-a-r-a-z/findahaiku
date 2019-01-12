2234e27292907fa4fe517fced6e99a3c47967e1e
f-a-r-a-z <benjithebigdog@gmail.com>
2019-01-12 20:13:45 +1000

    Added more test cases
    Also changed double quotes to single quotes to follow the AirBnb style
    guide

--- test.js
+++ test.js
@@ -4,27 +4,43 @@ const haiku = require('./index.js');
 describe('findahaiku', function() {
   describe('isHaiku()', function() {
     it('should detect a basic haiku (lowercase)', function() {
-      assert.equal(true, haiku.isHaiku("an old silent pond a frog jumps into the pond splash silence again"));
+      assert.equal(true, haiku.isHaiku('an old silent pond a frog jumps into the pond splash silence again'));
     });
 
     it('should detect a basic haiku (uppercase)', function() {
-      assert.equal(true, haiku.isHaiku("WIZARD AFTERNOON WHY A PRIVATE CRAZED CAR HOPS WATCHING THE VAMPIRE"));
+      assert.equal(true, haiku.isHaiku('WIZARD AFTERNOON WHY A PRIVATE CRAZED CAR HOPS WATCHING THE VAMPIRE'));
     });
 
     it('should detect a basic haiku (mixed case)', function() {
-      assert.equal(true, haiku.isHaiku("Pungent break of dAY A largE wEIrd AnTEATEr slEepS BeTrAYEd By tHE BirD"));
+      assert.equal(true, haiku.isHaiku('Pungent break of dAY A largE wEIrd AnTEATEr slEepS BeTrAYEd By tHE BirD'));
     });
 
     it('should reject a non-haiku with less than 17 syllables (lowercase)', function() {
-      assert.equal(false, haiku.isHaiku("this is not a haiku"));
+      assert.equal(false, haiku.isHaiku('this is not a haiku'));
     });
 
     it('should reject a non-haiku with more than 17 syllables (lowercase)', function() {
-      assert.equal(false, haiku.isHaiku("i am not a haiku but one day i could become one if i really tried hard and gave it my all"));
+      assert.equal(false, haiku.isHaiku('i am not a haiku but one day i could become one if i really tried hard and gave it my all'));
     });
 
     it('should reject a non-haiku with exactly 17 syllables (lowercase)', function() {
-      assert.equal(false, haiku.isHaiku("what if this is inappropriate extravaganza werewolf meat"));
+      assert.equal(false, haiku.isHaiku('what if this is inappropriate extravaganza werewolf meat'));
+    });
+
+    it('should ignore punctuation next to words', function() {
+      assert.equal(true, haiku.isHaiku('Hello, good morning?..... How are you today? Me too. See you later then!'));
+    });
+
+    it('should not ignore punctuation in the middle of words', function() {
+      assert.equal(true, haiku.isHaiku("I didn't do it, I swear! Please don't let this be! I won't forgive this..."));
+    });
+
+    it('should reject an empty string', function() {
+      assert.equal(false, haiku.isHaiku(''));
+    });
+
+    it('should reject no input', function() {
+      assert.equal(false, haiku.isHaiku(''));
     });
   });
 });
