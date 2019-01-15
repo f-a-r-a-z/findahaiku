848f993444a841fbfafec85430a831eebe8ec26c
f-a-r-a-z <farazshafiq99+git@gmail.com>
2019-01-15 20:21:17 +1000

    Ensure tests use new module name

--- test/isHaikuTest.js
+++ test/isHaikuTest.js
@@ -4,82 +4,82 @@ const haiku = require('.././index.js');
 describe('findahaiku', function() {
   describe('isHaiku(sentence)', function() {
     it('should detect a basic haiku (lowercase)', function() {
-      assert.equal(true, haiku.isHaiku('an old silent pond a frog jumps into the pond splash silence again'));
+      assert.equal(true, haiku.analyzeText('an old silent pond a frog jumps into the pond splash silence again').isHaiku);
     });
 
     it('should detect a basic haiku (uppercase)', function() {
-      assert.equal(true, haiku.isHaiku('WIZARD AFTERNOON WHY A PRIVATE CRAZED CAR HOPS WATCHING THE VAMPIRE'));
+      assert.equal(true, haiku.analyzeText('WIZARD AFTERNOON WHY A PRIVATE CRAZED CAR HOPS WATCHING THE VAMPIRE').isHaiku);
     });
 
     it('should detect a basic haiku (mixed case)', function() {
-      assert.equal(true, haiku.isHaiku('Pungent break of dAY A largE wEIrd AnTEATEr slEepS BeTrAYEd By tHE BirD'));
+      assert.equal(true, haiku.analyzeText('Pungent break of dAY A largE wEIrd AnTEATEr slEepS BeTrAYEd By tHE BirD').isHaiku);
     });
 
     it('should reject a basic haiku with words not in dictionary (lowercase)', function() {
-      assert.equal(false, haiku.isHaiku('yab yab yab yab yab yab yab yab yab yab yab yab yab yab yab yab yab'));
+      assert.equal(false, haiku.analyzeText('yab yab yab yab yab yab yab yab yab yab yab yab yab yab yab yab yab').isHaiku);
     });
 
     it('should reject a non-haiku with less than 17 syllables (lowercase)', function() {
-      assert.equal(false, haiku.isHaiku('this is not a haiku'));
+      assert.equal(false, haiku.analyzeText('this is not a haiku').isHaiku);
     });
 
     it('should reject a non-haiku with more than 17 syllables (lowercase)', function() {
-      assert.equal(false, haiku.isHaiku('i am not a haiku but one day i could become one if i really tried hard and gave it my all'));
+      assert.equal(false, haiku.analyzeText('i am not a haiku but one day i could become one if i really tried hard and gave it my all').isHaiku);
     });
 
     it('should reject a non-haiku with exactly 17 syllables (lowercase)', function() {
-      assert.equal(false, haiku.isHaiku('what if this is inappropriate extravaganza werewolf meat'));
+      assert.equal(false, haiku.analyzeText('what if this is inappropriate extravaganza werewolf meat').isHaiku);
     });
 
     it('should ignore punctuation next to words', function() {
-      assert.equal(true, haiku.isHaiku('Hello, good morning?..... How are you today? Me too. See you later then!'));
+      assert.equal(true, haiku.analyzeText('Hello, good morning?..... How are you today? Me too. See you later then!').isHaiku);
     });
 
     it('should not ignore punctuation in the middle of words', function() {
-      assert.equal(true, haiku.isHaiku("I didn't do it, I swear! Please don't let this be! I won't forgive this..."));
+      assert.equal(true, haiku.analyzeText("I didn't do it, I swear! Please don't let this be! I won't forgive this...").isHaiku);
     });
 
     it('should ignore newlines in the sentence', function() {
-      assert.equal(true, haiku.isHaiku("\nan old silent pond\n\na frog jumps into the pond\nsplash silence again"));
+      assert.equal(true, haiku.analyzeText("\nan old silent pond\n\na frog jumps into the pond\nsplash silence again").isHaiku);
     });
 
-    it('should ignore tabs and newlines in the sentence', function() {
-      assert.equal(true, haiku.isHaiku("\t\tan old silent pond\n\t\ta frog jumps into the pond\n\t\tsplash silence again"));
+    it('should ignore tabs, newlines and carriage returns in the sentence', function() {
+      assert.equal(true, haiku.analyzeText("\t\tan old silent\r\npond\n\t\ta frog jumps into the pond\n\t\tsplash silence again").isHaiku);
     });
 
     it('should ignore multiple spaces in the sentence', function() {
-      assert.equal(true, haiku.isHaiku(" an       old silent   pond  a frog  jumps into the pond   splash silence   again"));
+      assert.equal(true, haiku.analyzeText(" an       old silent   pond  a frog  jumps into the pond   splash silence   again").isHaiku);
     });
 
     it('should ignore punctuation by itself in the sentence', function() {
-      assert.equal(true, haiku.isHaiku("an old silent pond . a frog jumps into the pond . splash ! silence again ..."));
+      assert.equal(true, haiku.analyzeText("an old silent pond . a frog jumps into the pond . splash ! silence again ...").isHaiku);
     });
 
     it('should reject an empty string', function() {
-      assert.equal(false, haiku.isHaiku(''));
+      assert.equal(false, haiku.analyzeText('').isHaiku);
     });
 
     it('should throw error when given no input', function() {
       assert.throws(function() {
-        haiku.isHaiku();
+        haiku.analyzeText();
       }, TypeError);
     });
 
     it('should throw error when given numerical input', function() {
       assert.throws(function() {
-        haiku.isHaiku(91891);
+        haiku.analyzeText(91891);
       }, TypeError);
     });
 
     it('should throw error when given boolean input', function() {
       assert.throws(function() {
-        haiku.isHaiku(false);
+        haiku.analyzeText(false);
       }, TypeError);
     });
 
     it('should throw error when given object input', function() {
       assert.throws(function() {
-        haiku.isHaiku({"abc": 123});
+        haiku.analyzeText({"abc": 123});
       }, TypeError);
     });
   });
