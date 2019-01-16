73ab74b76088f67dd71c5f2dceab4e0d7a7dc097
f-a-r-a-z <farazshafiq99+f-a-r-a-z@gmail.com>
2019-01-16 15:53:55 +1000

    Refactor isHaiku tests
    Less repeated code makes it easier to add/read test cases

--- test/isHaikuTest.js
+++ test/isHaikuTest.js
@@ -1,62 +1,70 @@
 const assert = require('assert');
 const haiku = require('.././index.js');
 
+const testCases = [
+  { title: 'should detect a basic haiku (lowercase)',
+    input: 'an old silent pond a frog jumps into the pond splash silence again',
+    returns: true },
+
+  { title: 'should detect a basic haiku (uppercase)',
+    input: 'WIZARD AFTERNOON WHY A PRIVATE CRAZED CAR HOPS WATCHING THE VAMPIRE',
+    returns: true },
+
+  { title: 'should detect a basic haiku (mixed case)',
+    input: 'Pungent break of dAY A largE wEIrd AnTEATEr slEepS BeTrAYEd By tHE BirD',
+    returns: true },
+
+  { title: 'should reject a basic haiku with words not in dictionary (lowercase)',
+    input: 'yab yab yab yab yab yab yab yab yab yab yab yab yab yab yab yab yab',
+    returns: false },
+
+  { title: 'should reject a non-haiku with less than 17 syllables (lowercase)',
+    input: 'this is not a haiku',
+    returns: false },
+
+  { title: 'should reject a non-haiku with more than 17 syllables (lowercase)',
+    input: 'i am not a haiku but one day i could become one if i really tried hard and gave it my all',
+    returns: false },
+
+  { title: 'should reject a non-haiku with exactly 17 syllables (lowercase)',
+    input: 'what if this is inappropriate extravaganza werewolf meat',
+    returns: false },
+
+  { title: 'should ignore punctuation next to words',
+    input: 'Hello, good morning?..... How are you today? Me too. See you later then!',
+    returns: true },
+
+  { title: 'should not ignore punctuation in the middle of words',
+    input: "I didn't do it, I swear! Please don't let this be! I won't forgive this...",
+    returns: true },
+
+  { title: 'should ignore newlines in the sentence',
+    input: '\nan old silent pond\n\na frog jumps into the pond\nsplash silence again',
+    returns: true },
+
+  { title: 'should ignore tabs, newlines and carriage returns in the sentence',
+    input: '\t\tan old silent\r\npond\n\t\ta frog jumps into the pond\n\t\tsplash silence again',
+    returns: true },
+
+  { title: 'should ignore multiple spaces in the sentence',
+    input: ' an       old silent   pond  a frog  jumps into the pond   splash silence   again',
+    returns: true },
+
+  { title: 'should ignore punctuation by itself in the sentence',
+    input: 'an old silent pond . a frog jumps into the pond . splash ! silence again ...',
+    returns: true },
+
+  { title: 'should reject an empty string',
+    input: '',
+    returns: false },
+];
+
 describe('findahaiku', function() {
   describe('isHaiku(sentence)', function() {
-    it('should detect a basic haiku (lowercase)', function() {
-      assert.equal(true, haiku.analyzeText('an old silent pond a frog jumps into the pond splash silence again').isHaiku);
-    });
-
-    it('should detect a basic haiku (uppercase)', function() {
-      assert.equal(true, haiku.analyzeText('WIZARD AFTERNOON WHY A PRIVATE CRAZED CAR HOPS WATCHING THE VAMPIRE').isHaiku);
-    });
-
-    it('should detect a basic haiku (mixed case)', function() {
-      assert.equal(true, haiku.analyzeText('Pungent break of dAY A largE wEIrd AnTEATEr slEepS BeTrAYEd By tHE BirD').isHaiku);
-    });
-
-    it('should reject a basic haiku with words not in dictionary (lowercase)', function() {
-      assert.equal(false, haiku.analyzeText('yab yab yab yab yab yab yab yab yab yab yab yab yab yab yab yab yab').isHaiku);
-    });
-
-    it('should reject a non-haiku with less than 17 syllables (lowercase)', function() {
-      assert.equal(false, haiku.analyzeText('this is not a haiku').isHaiku);
-    });
-
-    it('should reject a non-haiku with more than 17 syllables (lowercase)', function() {
-      assert.equal(false, haiku.analyzeText('i am not a haiku but one day i could become one if i really tried hard and gave it my all').isHaiku);
-    });
-
-    it('should reject a non-haiku with exactly 17 syllables (lowercase)', function() {
-      assert.equal(false, haiku.analyzeText('what if this is inappropriate extravaganza werewolf meat').isHaiku);
-    });
-
-    it('should ignore punctuation next to words', function() {
-      assert.equal(true, haiku.analyzeText('Hello, good morning?..... How are you today? Me too. See you later then!').isHaiku);
-    });
-
-    it('should not ignore punctuation in the middle of words', function() {
-      assert.equal(true, haiku.analyzeText("I didn't do it, I swear! Please don't let this be! I won't forgive this...").isHaiku);
-    });
-
-    it('should ignore newlines in the sentence', function() {
-      assert.equal(true, haiku.analyzeText("\nan old silent pond\n\na frog jumps into the pond\nsplash silence again").isHaiku);
-    });
-
-    it('should ignore tabs, newlines and carriage returns in the sentence', function() {
-      assert.equal(true, haiku.analyzeText("\t\tan old silent\r\npond\n\t\ta frog jumps into the pond\n\t\tsplash silence again").isHaiku);
-    });
-
-    it('should ignore multiple spaces in the sentence', function() {
-      assert.equal(true, haiku.analyzeText(" an       old silent   pond  a frog  jumps into the pond   splash silence   again").isHaiku);
-    });
-
-    it('should ignore punctuation by itself in the sentence', function() {
-      assert.equal(true, haiku.analyzeText("an old silent pond . a frog jumps into the pond . splash ! silence again ...").isHaiku);
-    });
-
-    it('should reject an empty string', function() {
-      assert.equal(false, haiku.analyzeText('').isHaiku);
+    testCases.forEach(function(testCase) {
+      it(testCase.title, function() {
+        assert.equal(haiku.analyzeText(testCase.input).isHaiku, testCase.returns);
+      });
     });
 
     it('should throw error when given no input', function() {
