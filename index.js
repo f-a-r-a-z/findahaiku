ed80bc26aab140ff0a1974c79f913f92e901f810
f-a-r-a-z <farazshafiq99+f-a-r-a-z@gmail.com>
2019-03-31 16:37:48 +1000

    Remove type check

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ module.exports = {
 const pronounciations = require('cmu-pronouncing-dictionary');
 
 function analyzeText(sentence) {
-  if (!isString(sentence)) throw new TypeError('findahaiku.analyzeText expects string input, recieved ' + typeof sentence);
-
   let result = { isHaiku: false, formattedHaiku: '' };
 
   const wordArray = sentence.split(' ') || [];
